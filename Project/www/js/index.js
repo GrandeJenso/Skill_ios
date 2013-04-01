605f071018fd2cf21ad7134d7a53a4a2505c5d3f
Jens Lund <lund.jens@hotmail.com>
2013-04-01 12:06:31 +0200

    Push notification + ikoner

--- Project/www/js/index.js
+++ Project/www/js/index.js
@@ -16,41 +16,32 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+document.addEventListener("deviceready", onDeviceReady, false);
 
+var xml_all;
+var detailed_div;
+var job_id;
 
-$(document).on("mobileinit", function(){
-               $.mobile.defaultPageTransition = "slide";
-               // Setting #container div as a jqm pageContainer
-               $.mobile.pageContainer = $('#container');
+$(document).on('pageinit',"body",function()
+               {
+                detailed_div=$("div#detailed_job_content ul");
                });
-/*$(function() {
-  $("#no_internet #refresh_button").click(function(){
-                                          if(!navigator.onLine)
-                                          {
-                                          alert("No connection to internet");
-                                          }
-                                          else
-                                          {
-                                          $.mobile.changePage("#jobs",{ transition: "none"});
-                                          }
-                                          });
-  });*/
-
 
 $(document).on('pageshow', "#jobs", function()
                {
-               var jobs_div = $('div.wrapper ul#job');
+               if(!xml_all)
+               {
+               var jobs_div = $('div#jobs_content ul#job');
                jobs_div.text('');
-               $.mobile.loading('show');
                $.ajax({
                       type: "GET",
-                      url: "http://cv.skill.se/cv/rss.jsp?format=mtrxml&allads=1",
+                      url: "http://cv.skill.se/cv/rss.jsp?format=mtrxml&allads=1&fullad=1",
                       dataType: "xml",
                       success: parseXml,
                       error: errorMessage
                       });
                
-               
+               }
                function parseXml(xml)
                {
                
@@ -66,65 +57,173 @@ $(document).on('pageshow', "#jobs", function()
                                        var area = $(this).find('area').text();
                                        var assignment_type = $(this).find('assignmentType').text();
                                        
-                                       jobs_div.append('<li class="listelement" data-id= "' + id + '" ><a data-transition="slide"><img src="' + img +'" class="ui-li-thumb"><p class="ui-li-heading">' + title + '</p><p class="ui-li-desc">'+area+','+assignment_type+'</br>' +location+', '+date+'</p></a></li>');
+                                       jobs_div.append('<li class="listelement" data-id= "' + id + '" ><a data-transition="slide" ><img src="' + img +'" class="ui-li-thumb"><p class="ui-li-heading">' + title + '</p><p class="ui-li-desc">'+area+','+assignment_type+'</br>' +location+', '+date+'</p></a></li>');
                                        });
-               $('div.wrapper ul#job').listview("refresh");
-               $.mobile.loading('hide');
-               }
+               $('div#jobs_content ul#job').listview('refresh');
                
-               function errorMessage(error)
-               {
-                    alert('Du måste vara uppkopplad på internet för att se denna del av appen.');
+               $('li.listelement').click(function(){
+                                         
+                                        detailed_div.text('');
+                                        job_id = $(this).data('id');
+                                         var job = $(xml_all).find("job[id="+job_id+"]")
+                                         var title = job.find('title').text();
+                                         var company = job.find('company name').text();
+                                         var description = job.find('description').text();
+                                         var link = job.find('applicationMethods link').attr('href');
+                                         $('#detailed_job_view div.header h1').html(company);
+                                         detailed_div.append("<h3>"+title+"</h3>");
+                                         detailed_div.append("<p>"+description+"</p>");
+                                         detailed_div.append('<a data-role="button" id="job_link">Ansök</a>');
+                                            
+
+                                        $.mobile.changePage('#detailed_job_view',{transition:"slide"});
+                                        
+                });
+               
                }
 });
 
-/*document.addEventListener("deviceready", onDeviceReady, false);
+
+function errorMessage(error)
+{
+    navigator.notification.alert(
+                                 'Du måste vara ansluten till internet för att se denna del av applikationen',  // message
+                                 alertDismissed,         // callback
+                                 'Ingen internetanslutning',            // title
+                                 'OK'                  // buttonName
+                                 );
+}
+
+$(document).on('pageshow', '#detailed_job_view', function()
+               {
+               $('#detailed_job_content').iscrollview("refresh");
+               $('#detailed_job_view').trigger("create");
+               $('button#job_link').click(function(){
+                                         ref = window.open(link,'_blank', 'location=no');
+                                         
+                                         
+                                         });
+               });
 
 // Cordova is loaded and it is now safe to make calls Cordova methods
 //
+function alertDismissed(){
+    
+}
 function onDeviceReady() {
     checkConnection();
+
 }
 
 function checkConnection() {
     if(!navigator.onLine)
     {
-        alert("No connection to internet");
-        $.mobile.changePage("#no_internet",{ transition: "none"});
+        navigator.notification.alert(
+                                     'Du måste vara ansluten till internet för att se denna del av applikationen',  // message
+                                     alertDismissed,         // callback
+                                     'Ingen internetanslutning',            // title
+                                     'OK'                  // buttonName
+                                     );
+        //$.mobile.changePage("#no_internet",{ transition: "none"});
     }
-}*/
+}
 
 
 
 var app = {
     // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
+initialize: function() {
+    this.bindEvents();
+},
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
+bindEvents: function() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+},
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        
-    },
+onDeviceReady: function() {
+    app.receivedEvent('deviceready');
+},
+tokenHandler:function(msg) {
+    console.log("Token Handler " + msg);
+    //alert(msg);
+},
+errorHandler:function(error) {
+    console.log("Error Handler  " + error);
+    alert(error);
+},
+    // result contains any message sent from the plugin call
+successHandler: function(result) {
+    alert('Success! Result = '+result)
+},
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+receivedEvent: function(id) {
+    var pushNotification = window.plugins.pushNotification;
+    // TODO: Enter your own GCM Sender ID in the register call for Android
+    if (device.platform == 'android' || device.platform == 'Android') {
+        pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"1011544002093","ecb":"app.onNotificationGCM"});
     }
-};
+    else {
+        pushNotification.register(this.tokenHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
+    }
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
+    
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+    
+    console.log('Received Event: ' + id);
+},
+    // iOS
+onNotificationAPN: function(event) {
+    var pushNotification = window.plugins.pushNotification;
+    console.log("Received a notification! " + event.alert);
+    console.log("event sound " + event.sound);
+    console.log("event badge " + event.badge);
+    console.log("event " + event);
+    if (event.alert) {
+        navigator.notification.alert(event.alert);
+    }
+    if (event.badge) {
+        console.log("Set badge on  " + pushNotification);
+        pushNotification.setApplicationIconBadgeNumber(this.successHandler, event.badge);
+    }
+    if (event.sound) {
+        var snd = new Media(event.sound);
+        snd.play();
+    }
+},
+    // Android
+onNotificationGCM: function(e) {
+    switch( e.event )
+    {
+        case 'registered':
+            if ( e.regid.length > 0 )
+            {
+                // Your GCM push server needs to know the regID before it can push to this device
+                // here is where you might want to send it the regID for later use.
+                alert('registration id = '+e.regid);
+            }
+            break;
+            
+        case 'message':
+            // this is the actual push notification. its format depends on the data model
+            // of the intermediary push server which must also be reflected in GCMIntentService.java
+            alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+            
+        case 'error':
+            alert('GCM error = '+e.msg);
+            break;
+            
+        default:
+            alert('An unknown GCM event has occurred');
+            break;
+    }
+}};
